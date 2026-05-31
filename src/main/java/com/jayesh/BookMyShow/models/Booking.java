a56a24f43533266ad9360c40b9c5e9c84286f3cd
Jayesh <jayeshdongre31@gmail.com>
2026-05-31 21:25:47 +0530

    add annotations for the models and dependencies for db connectivity

--- src/main/java/com/jayesh/BookMyShow/models/Booking.java
+++ src/main/java/com/jayesh/BookMyShow/models/Booking.java
@@ -1,12 +1,34 @@
 package com.jayesh.BookMyShow.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.List;
 
+@Getter
+@Setter
+@Entity
 public class Booking extends BaseModel{
+    @ManyToOne
     private User user;
     private Date bookingDate;
+    @OneToMany
     private List<Payment> payments;
+    @ManyToOne
     private Show show;
+    @Enumerated(value = EnumType.STRING)
     private BookingStatus bookingStatus;
+
+    @OneToMany
+    private List<ShowSeat> showSeats;
+    private int amount;
 }
+
+// Cardinality:
+//User  1 : M      Booking
+// Booking  1 : M    Payments
+// Bookign M : 1  SHow
+
+// Sign up a user
--- src/main/java/com/jayesh/BookMyShow/models/Show.java
+++ src/main/java/com/jayesh/BookMyShow/models/Show.java
@@ -3,16 +3,28 @@ package com.jayesh.BookMyShow.models;
 import java.util.Date;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity(name = "shows")
 public class Show extends BaseModel{
-
+    @ManyToOne
     private Theatre theatre;
+    @ManyToOne
     private Screen screen;
+    @ManyToOne
     private Movie movie;
     private String startTime;
     private String endTime;
     private Date showDate;
+    @OneToMany
     private List<ShowSeat> seats;
+    @OneToMany
     private List<ShowSeatType> seatTypes;
 
 }
