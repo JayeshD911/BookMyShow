94ac352a3aaf725f02cca80d32b486216de67d53
Jayesh <jayeshdongre31@gmail.com>
2026-06-06 20:48:12 +0530

    add CommandLineRunner to test working of user service

--- src/main/java/com/jayesh/BookMyShow/BookMyShowApplication.java
+++ src/main/java/com/jayesh/BookMyShow/BookMyShowApplication.java
@@ -1,10 +1,28 @@
 package com.jayesh.BookMyShow;
 
+import com.jayesh.BookMyShow.controllers.UserController;
+import com.jayesh.BookMyShow.dtos.SignUpUserRequestDTO;
+import com.jayesh.BookMyShow.dtos.SignUpUserResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookMyShowApplication {
+public class BookMyShowApplication implements CommandLineRunner {
+	@Autowired
+	UserController userController;
+
+	@Override
+	public void run(String... args) throws Exception {
+		SignUpUserRequestDTO request = new SignUpUserRequestDTO();
+		request.setName("Jayesh");
+		request.setEmail("jaydec1997@gmail.com");
+		request.setPassword("password");
+		SignUpUserResponseDTO response= userController.signUpUser(request);
+		System.out.println(response.getMessage());
+		System.out.println(response.getStatus());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMyShowApplication.class, args);
